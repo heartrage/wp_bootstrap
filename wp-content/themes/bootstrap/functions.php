404f327db9357d3b564f888ca8cc3b1735c02215
Andrey <vizual2005@yandex.ru>
2015-04-01 03:41:40 +0400

    Добавил стили и скрипты в functions.php

--- wp-content/themes/bootstrap/functions.php
+++ wp-content/themes/bootstrap/functions.php
@@ -1,6 +1,21 @@
 <?php
+/**
+Загружаемые стили и скрипты
+ *
+ */
+function load_style_script() {
+    wp_enqueue_style('style.css', get_template_directory_uri(). '/style.css');
+    wp_enqueue_style('menu.css', get_template_directory_uri() . '/css/menu.css');
+    wp_enqueue_style('bootstrap.css', get_template_directory_uri(). '/css/bootstrap.css');
+    wp_enqueue_script('jquery.min.js', 'https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js');
+    wp_enqueue_script('menu.js', get_template_directory_uri() . '/js/menu.js');
+}
 
-
+/**
+Загружаем стили и скрипты
+ *
+ */
+add_action('wp_enqueue_scripts', 'load_style_script');
 
 
 
