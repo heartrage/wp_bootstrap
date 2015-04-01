e83184ef80432ac26b3256d40da07237c651ff47
Andrey <vizual2005@yandex.ru>
2015-04-01 16:30:59 +0400

    редактирование

--- wp-content/themes/bootstrap/functions.php
+++ wp-content/themes/bootstrap/functions.php
@@ -2,9 +2,11 @@
 /*******************Загружаемые стили и скрипты************************************/
 
 function load_style_script() {
+/*******style***********/
     wp_enqueue_style('style.css', get_template_directory_uri(). '/style.css');
     wp_enqueue_style('menu.css', get_template_directory_uri() . '/css/menu.css');
     wp_enqueue_style('bootstrap.css', get_template_directory_uri(). '/css/bootstrap.css');
+/******script********/
     wp_enqueue_script('jquery.min.js', 'https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js');
     wp_enqueue_script('menu.js', get_template_directory_uri() . '/js/menu.js');
 }
